cf506654c8e369a9c22c64fb6c8e151acdeba97f
alokik98 <alorshsaxena12@gmail.com>
2022-04-04 11:36:40 +0530

    Added the Random generation of pass on checked checkboxes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Homepage from "./components/Homepage";
 
 function App() {
   return (
-    <div className="flex m-0 justify-center items-center bg-[#eff6f4] h-screen md:text-3xl sm:text-2xl lg:text-4xl 3xl:text-6xl">
+    <div className="flex m-0 justify-center items-center bg-[#def2ed] h-screen md:text-3xl sm:text-2xl lg:text-4xl 3xl:text-6xl">
       <Homepage />
     </div>
   );
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./Homepage.css";
+import copy_clipboard from "./copy_icon.png";
 
 function Homepage() {
-  const [InputValue, setInputValue] = useState("0");
+  const [inputValue, setInputValue] = useState("0");
   function getValue(event) {
     setInputValue(event.target.value);
   }
@@ -12,12 +13,23 @@ function Homepage() {
   const allUniqueChars = [..."~!@#$%^&*()_+-=[]\\{}|;:'\",./<>?"];
   const allNumbers = [..."0123456789"];
 
-  const base = [
-    ...allCapsAlpha,
-    ...allNumbers,
-    ...allLowerAlpha,
-    ...allUniqueChars,
-  ];
+  const [base, setBase] = useState([]);
+  // console.log(base);
+
+  const handleCopy = () => {
+    var text = document.getElementById("myText");
+    var copy = text.textContent;
+    if (copy !== "") navigator.clipboard.writeText(copy);
+    document.getSelection().removeAllRanges();
+  };
+
+  // const [isChecked, setIsChecked] = useState(false);
+
+
+  // const handleOnChange = () => {
+  //   setIsChecked(!isChecked);
+  //   console.log(isChecked);
+  // };
 
   const generator = (base, len) => {
     return [...Array(len)]
@@ -25,38 +37,56 @@ function Homepage() {
       .join("");
   };
 
+
   function a() {
-    var string = (generator(base, parseInt(InputValue)));
-    return { __html: string <= 0 ? "" : string }
+    var string = generator(base, parseInt(inputValue));
+    return { __html: string <= 0 ? "" : string };
   }
 
+  var checkboxes = document.querySelectorAll("input[type=checkbox]");
 
-  const handleCopy = () => {
-    var text = document.getElementById("myText");
-    var copy = text.textContent;
-    if (copy !== "") navigator.clipboard.writeText(copy);
-    document.getSelection().removeAllRanges();
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].onclick = isChecked;
   }
 
-  const [isChecked, setIsChecked] = useState(false);
-  const handleOnChange = () => {
-    setIsChecked(!isChecked);
-    console.log(isChecked)
-  };
+  function isChecked(event) {
+    var checkbox = event.target;
+    if (checkbox.checked === true) { addElementsInArray(checkbox.id); }
+    if (checkbox.checked === false) { removeElements(checkbox) }
+  }
+
+  function addElementsInArray(id) {
+    if (id === "cb1") {
+      setBase(base => [...base, ...allUniqueChars]);
+    }
+    if (id === "cb2") {
+      setBase(base => [...base, ...allNumbers]);
+    }
+    if (id === "cb3") {
+      setBase(base => [...base, ...allLowerAlpha]);
+    }
+    if (id === "cb4") {
+      setBase(base => [...base, ...allCapsAlpha]);
+    }
+  }
+
+  function removeElements(e) {
+    console.log(e)
+  }
+
 
   return (
     <div className="flex flex-col">
       <div className="h-full flex items-center">
         <span
           id="myText"
-          className="min-w-full h-16 bg-white shadow-[
-#e1ece4] rounded-xl font-noto-sans" dangerouslySetInnerHTML={a()}
-        >
-        </span>
+          className="min-w-full h-20 bg-white drop-shadow-xl rounded-xl leading-loose pl-6 whitespace-pre-line font-noto-sans"
+          dangerouslySetInnerHTML={a()}
+        ></span>
         <img
-          src="copy_icon.png"
+          src={copy_clipboard}
           alt=""
-          className="w-10 h-max mx-2 cursor-pointer"
+          className="w-10 h-max mx-2 cursor-pointer bg-inherit"
           onClick={handleCopy}
         />
       </div>
@@ -72,24 +102,29 @@ function Homepage() {
           onChange={getValue}
         />
         <label id="display_range" className="text-lg m-2 font-noto-sans">
-          {InputValue} Characters
+          {inputValue} Characters
         </label>
       </div>
       <div className="pt-3">
         <label className="flex mt-2 mb-4 font-noto-sans">
-          <input type="checkbox" name="checkbox" id="cb1" checked={isChecked} onChange={handleOnChange} />
+          <input
+            type="checkbox"
+            name="checkbox"
+            id="cb1"
+            onChange={isChecked}
+          />
           <b>Include Symbols</b>&nbsp; (@#$%)
         </label>
         <label className="flex mt-2 mb-4 font-noto-sans">
-          <input type="checkbox" name="checkbox" id="cb2" className="h-5 w-5" />
+          <input type="checkbox" name="checkbox" id="cb2" className="h-5 w-5" onChange={isChecked} />
           <b>Include Numbers</b> &nbsp; (1234)
         </label>
         <label className="flex mt-2 mb-4 font-noto-sans">
-          <input type="checkbox" name="checkbox" id="cb3" className="h-5 w-5" />
+          <input type="checkbox" name="checkbox" id="cb3" className="h-5 w-5" onChange={isChecked} />
           <b>Include Lowercase Characters </b> &nbsp; (abcd)
         </label>
         <label className="flex mt-2 mb-4 font-noto-sans">
-          <input type="checkbox" name="checkbox" id="cb4" className="h-5 w-5" />
+          <input type="checkbox" name="checkbox" id="cb4" className="h-5 w-5" onChange={isChecked} />
           <b>Include Uppercase Characters </b> &nbsp; (ABCD)
         </label>
       </div>
